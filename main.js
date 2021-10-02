d4509eafe26927ec29570d223dac6d26b2b103c6
Nishchal <71500515+Nish251103@users.noreply.github.com>
2021-10-02 11:35:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+// typing texi effect
+async function init () {
+  const node = document.querySelector("#type-text")
+
+  await sleep(1000)
+  node.innerText = ""
+  await node.type("I'm ")
+
+
+  while (true) {
+
+    await node.type("a Developer")
+    await sleep(2000)
+    await node.delete("a Developer")
+
+    await node.type("a Blogger")
+    await sleep(2000)
+    await node.delete("a Blogger")
+
+    await node.type("a Musician")
+    await sleep(2000)
+    await node.delete("a Musician")
+
+  }
+}
+
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+
+class TypeAsync extends HTMLSpanElement {
+  get typeInterval () {
+    const randomMs = 100 * Math.random()
+    return randomMs < 50 ? 10 : randomMs
+  }
+
+  async type (text) {
+    for (let character of text) {
+      this.innerText += character
+      await sleep(this.typeInterval)
+    }
+  }
+
+  async delete (text) {
+    for (let character of text) {
+      this.innerText = this.innerText.slice(0, this.innerText.length -1)
+      await sleep(this.typeInterval)
+    }
+  }
+}
+
+customElements.define('type-async', TypeAsync, { extends: 'span' })
+
+
+init()
+
+// .............................................................................................
