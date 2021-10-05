1f32a38baa6410dccb3e42a35337cfbfae030bd3
Anushka <anushka.garg.mat20@itbhu.ac.in>
2021-10-05 22:47:31 +0530

    Added scroll to top button

--- main.js
+++ main.js
@@ -52,6 +52,17 @@ class TypeAsync extends HTMLSpanElement {
 
 customElements.define('type-async', TypeAsync, { extends: 'span' })
 
+var scrolBtn = document.querySelector("#myDIV");
+      window.addEventListener("scroll", function () {
+        let offset;
+        offset = window.pageYOffset;
+        if (offset > 300) {
+          scrolBtn.style.display = "block";
+        }
+        if (offset < 300) {
+          scrolBtn.style.display = "none";
+        }
+      });
 
 init()
 
